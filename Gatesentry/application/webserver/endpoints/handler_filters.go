772fac0c07c8d3a203a8985123f76e32c5510bb0
arxndev <arun.kumar@adypu.edu.in>
2026-06-18 15:43:43 +0530

    Add Gatesentry/application/webserver/endpoints/handler_filters.go

---
+++ Gatesentry/application/webserver/endpoints/handler_filters.go
@@ -0,0 +1,55 @@
+package gatesentryWebserverEndpoints
+
+import (
+	"encoding/json"
+	"log"
+
+	gatesentryFilters "bitbucket.org/abdullah_irfan/gatesentryf/filters"
+)
+
+var GetAllFilters = func(Filters *[]gatesentryFilters.GSFilter) interface{} {
+	filterList := []gatesentryFilters.GSAPIStructFilter{}
+	for _, v := range *Filters {
+		filterList = append(filterList, gatesentryFilters.GSAPIStructFilter{
+			Id:      v.Id,
+			Name:    v.FilterName,
+			Handles: v.Handles,
+			Entries: v.FileContents,
+		})
+	}
+	return filterList
+}
+
+var GetSingleFilter = func(requestedId string, Filters *[]gatesentryFilters.GSFilter) interface{} {
+	filterList := []gatesentryFilters.GSAPIStructFilter{}
+	for _, v := range *Filters {
+		if v.Id == requestedId {
+			filterList = append(filterList, gatesentryFilters.GSAPIStructFilter{
+				Id:      v.Id,
+				Name:    v.FilterName,
+				Handles: v.Handles,
+				Entries: v.FileContents,
+			})
+		}
+	}
+	return filterList
+}
+
+var PostSingleFilter = func(requestedId string, dataReceived []gatesentryFilters.GsFilterLine, Filters *[]gatesentryFilters.GSFilter) interface{} {
+	for _, v := range *Filters {
+		if v.Id == requestedId {
+			data, err := json.MarshalIndent(dataReceived, "", "  ")
+			if err != nil {
+				return struct {
+					Response string `json:"response"`
+					Error    string `json:"error"`
+				}{Error: err.Error(), Response: "Error!"}
+			}
+			log.Println("Data received = " + string(data))
+			gatesentryFilters.GSSaveFilterFile(v.FileName, string(data))
+		}
+	}
+	return struct {
+		Response string `json:"response"`
+	}{"Ok!"}
+}
