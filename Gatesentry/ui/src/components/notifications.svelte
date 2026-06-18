84d6230499132a7a89f1297aac478f89d3efdfbf
arxndev <arun.kumar@adypu.edu.in>
2026-06-18 15:43:51 +0530

    Add Gatesentry/ui/src/components/notifications.svelte

---
+++ Gatesentry/ui/src/components/notifications.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { afterUpdate } from "svelte";
+  import { notificationstore } from "../store/notifications";
+  import { ToastNotification } from "carbon-components-svelte";
+
+  let notifications = [];
+
+  afterUpdate(() => {
+    notifications = $notificationstore;
+  });
+</script>
+
+<div
+  style="position: absolute; right:0; bottom: 0; text-align:left;z-index:10000"
+>
+  {#if notifications.length > 0}
+    {#each notifications as notification}
+      <ToastNotification
+        kind={notification.kind}
+        title={notification.title}
+        subtitle={notification.subtitle}
+        timeout={notification.timeout}
+        on:close={(e) => {
+          notificationstore.remove(notification);
+        }}
+      />
+    {/each}
+  {/if}
+</div>
