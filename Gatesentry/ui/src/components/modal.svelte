b620b6ddf5447090921a230adcb519c61f544810
arxndev <arun.kumar@adypu.edu.in>
2026-06-18 15:43:51 +0530

    Add Gatesentry/ui/src/components/modal.svelte

---
+++ Gatesentry/ui/src/components/modal.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { Modal, ModalBody } from "carbon-components-svelte";
+  import { _ } from "svelte-i18n";
+  export let open = false;
+  export let on: { close: () => void } = { close: () => {} };
+  export let title = "";
+  export let children = null;
+  export let hasForm = false;
+  export let shouldSubmitOnEnter = false;
+</script>
+
+{#if open}
+  <div class="gatesentry-modal">
+    <Modal
+      {shouldSubmitOnEnter}
+      {hasForm}
+      bind:open
+      on:close={on.close}
+      modalHeading={title}
+      primaryButtonDisabled={true}
+      ><slot />
+    </Modal>
+  </div>
+{/if}
