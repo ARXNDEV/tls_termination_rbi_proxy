7fdb90244b3f15faf128998df68e736a64ffc99f
arxndev <arun.kumar@adypu.edu.in>
2026-06-18 15:43:52 +0530

    Add Gatesentry/ui/src/routes/services/services.svelte

---
+++ Gatesentry/ui/src/routes/services/services.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Breadcrumb, BreadcrumbItem, Toggle } from "carbon-components-svelte";
+  import { _ } from "svelte-i18n";
+  import ToggleComponent from "../../components/toggle.svelte";
+</script>
+
+<Breadcrumb style="margin-bottom: 10px;">
+  <BreadcrumbItem href="/">Dashboard</BreadcrumbItem>
+  <BreadcrumbItem>Toggle Services</BreadcrumbItem>
+</Breadcrumb>
+<h2>Toggle Services</h2>
+
+<br />
+
+<ToggleComponent
+  settingName="enable_dns_server"
+  label="DNS Server"
+  labelA="Not Running"
+  labelB="Running"
+/>
