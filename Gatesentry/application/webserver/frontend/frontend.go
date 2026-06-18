cc364400336de71e001baa1b80d8f085533d4e46
arxndev <arun.kumar@adypu.edu.in>
2026-06-18 15:43:44 +0530

    Add Gatesentry/application/webserver/frontend/frontend.go

---
+++ Gatesentry/application/webserver/frontend/frontend.go
@@ -0,0 +1,54 @@
+package gatesentryWebserverFrontend
+
+import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
+)
+
+//go:embed files
+var build embed.FS
+
+func GetBlockPageMaterialUIStylesheet() []byte {
+	cssData, err := fs.ReadFile(build, "files/material.css")
+	if err != nil {
+		return nil
+	}
+	return cssData
+
+}
+
+func GetIndexHtml() []byte {
+	indexData, err := fs.ReadFile(build, "files/index.html")
+	if err != nil {
+		return nil
+	}
+	return indexData
+
+}
+
+func GetFileSystem(dir string, fsys fs.FS) http.FileSystem {
+	// if useOS {
+	// 	log.Println("[Webserver] using live mode")
+	// 	return http.FS(os.DirFS(dir))
+	// }
+
+	log.Print("[Webserver] using embed mode")
+	fsys, err := fs.Sub(fsys, dir)
+	if err != nil {
+		panic(err)
+	}
+
+	return http.FS(fsys)
+}
+
+func GetFSHandler() http.FileSystem {
+	log.Print("[Webserver] using embed mode")
+	fsys, err := fs.Sub(build, "files")
+	if err != nil {
+		panic(err)
+	}
+
+	return http.FS(fsys)
+}
