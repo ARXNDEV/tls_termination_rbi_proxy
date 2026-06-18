0290c7d70f4215e246b3608e19813aea8c5bab46
arxndev <arun.kumar@adypu.edu.in>
2026-06-18 15:43:51 +0530

    Add Gatesentry/ui/src/components/toggle.svelte

---
+++ Gatesentry/ui/src/components/toggle.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  import { Loading, Toggle } from "carbon-components-svelte";
+  export let settingName = "enable_https_filtering";
+  export let label = "Label";
+  export let labelA = "On";
+  export let labelB = "Off";
+  import { _ } from "svelte-i18n";
+  import { store } from "../store/apistore";
+  import { onMount } from "svelte";
+  import { notificationstore } from "../store/notifications";
+  import { createNotificationSuccess } from "../lib/utils";
+
+  export let settingValue = "";
+  export let preClickEvent = null;
+  export let hide = false;
+  export let noNotification = false;
+  const loadAPIData = () => {
+    $store.api.doCall("/settings/" + settingName).then((json) => {
+      settingValue = json.Value;
+    });
+  };
+
+  const toggleSettingStatus = async () => {
+    if (preClickEvent) {
+      await preClickEvent();
+    }
+    const url = "/settings/" + settingName;
+    var datatosend = {
+      key: settingName,
+      value: settingValue == "true" ? "false" : "true",
+    };
+
+    $store.api.doCall(url, "post", datatosend).then(function (json) {
+      loadAPIData();
+      if (noNotification) {
+        return;
+      }
+      notificationstore.add(
+        createNotificationSuccess(
+          {
+            title: $_("Success"),
+            subtitle: $_("Setting updated"),
+          },
+          $_,
+        ),
+      );
+    });
+  };
+
+  onMount(() => {
+    loadAPIData();
+  });
+
+  $: {
+    loadAPIData();
+  }
+</script>
+
+<span>
+  {#if hide == true}{:else if settingValue == ""}
+    <Loading />
+  {:else}
+    <Toggle
+      labelText={label}
+      {labelA}
+      {labelB}
+      toggled={settingValue == "true"}
+      on:change={toggleSettingStatus}
+    />
+  {/if}
+</span>
