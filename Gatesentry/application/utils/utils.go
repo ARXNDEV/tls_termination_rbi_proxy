07ba45ba967d1ac5c3ff9451d6c1d2f0d3076c64
arxndev <arun.kumar@adypu.edu.in>
2026-06-18 15:43:43 +0530

    Add Gatesentry/application/utils/utils.go

---
+++ Gatesentry/application/utils/utils.go
@@ -0,0 +1,47 @@
+package gatesentry2utils
+
+import (
+	// "bytes"
+
+	// "io/ioutil"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+var proxyAuthorizationHeader = "Proxy-Authorization"
+
+func RemoveAuthorizationHeader(req *http.Request) {
+	req.Header.Del(proxyAuthorizationHeader)
+}
+
+func Int64toString(u int64) string {
+	return strconv.FormatInt(u, 10)
+}
+
+func RandomString(n int) string {
+	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	const (
+		letterIdxBits = 6                    // 6 bits to represent a letter index
+		letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+		letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+	)
+
+	var src = rand.NewSource(time.Now().UnixNano())
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
