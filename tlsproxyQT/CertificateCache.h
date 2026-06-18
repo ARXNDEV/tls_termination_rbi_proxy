8adb899d40a7733a4d3906569fff4015fbdc7d4d
arxndev <arun.kumar@adypu.edu.in>
2026-06-18 15:43:54 +0530

    Add tlsproxyQT/CertificateCache.h

---
+++ tlsproxyQT/CertificateCache.h
@@ -0,0 +1,18 @@
+// #pragma once
+// #include <qmap.h>
+// #include <QtNetwork/QSslCertificate.h>
+
+// class CertificateCacheEntry {
+// 	QSslCertificate _certificate; 
+// 	QTime _last_time_accessed; 
+// 	QTime _last_cleaned;
+// 	QTime _added_time;
+
+// };
+// class CertificateCache
+// {
+// public: 
+// 	QMap<QString, CertificateCacheEntry> _cert_cache;
+
+// };
+
