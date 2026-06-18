81c7e5785a167bf574e7ced25cee1907b0b57eb7
arxndev <arun.kumar@adypu.edu.in>
2026-06-18 15:43:52 +0530

    Add Gatesentry/ui/src/routes/settings/settings.svelte

---
+++ Gatesentry/ui/src/routes/settings/settings.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { _ } from "svelte-i18n";
+
+  import ConnectedGeneralSettingInput from "../../components/connectedGeneralSettingInputs.svelte";
+  import HttpsToggle from "../../components/httpsToggle.svelte";
+  import ConnectedCertificateComposed from "../../components/connectedCertificateComposed.svelte";
+  import ConnectedSettingInput from "../../components/connectedSettingInput.svelte";
+  import { Breadcrumb, BreadcrumbItem } from "carbon-components-svelte";
+</script>
+
+<Breadcrumb style="margin-bottom: 10px;">
+  <BreadcrumbItem href="/">Dashboard</BreadcrumbItem>
+  <BreadcrumbItem>Settings</BreadcrumbItem>
+</Breadcrumb>
+
+<h2>Settings</h2>
+
+<br />
+
+<ConnectedGeneralSettingInput
+  keyName="log_location"
+  title={$_("Log Location")}
+  labelText={$_("Log Location")}
+  type="text"
+  helperText=""
+/>
+<br />
+<ConnectedGeneralSettingInput
+  keyName="admin_username"
+  helperText={""}
+  type="text"
+  title={$_("Admin username")}
+  labelText={$_("Admin username")}
+  disabled={true}
+/>
+
+<HttpsToggle />
+
+<ConnectedCertificateComposed />
